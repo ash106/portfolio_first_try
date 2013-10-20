f544aa575bcbfeea4a5747b787bca73c466b3693
Alex Howington <alex.howington@gmail.com>
2013-10-20 00:56:50 -0600

    Create UserMailer and add welcome email after user signup

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  default from: "noreply@devpointlabs.com"
+  #Call email using: UserMailer.welcome(user).deliver
+  def welcome(user)
+    @user = user
+    mail(to: @user.email, subject: "Welcome to Alex's Portfolio!")
+  end
+end
