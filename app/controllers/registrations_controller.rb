e6f467f0ee156d62a64ade3321eca5b23c41d915
Alex Howington <alex.howington@gmail.com>
2013-11-13 02:54:13 -0700

    Add user picture to home page, also add ability to edit avatar after user creation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,13 @@ class RegistrationsController < Devise::RegistrationsController
     resource.build_picture
     respond_with self.resource
   end
+
+  def edit
+    if current_user.picture == nil
+      self.resource.build_picture
+    else
+      self.resource.picture = current_user.picture
+    end
+    render :edit
+  end
 end
